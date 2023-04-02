c30fdf4586f9bd943a73cf0c547951aa97e69ae9
Gluttony-sw <farhansw.off@gmail.com>
2023-04-02 17:02:51 +0700

    Hands On OOP

---
+++ Hands On OOP/Vehicle.hpp
@@ -0,0 +1,44 @@
+#define __VEHICLE_HPP__
+#include <iostream>
+using namespace std;
+
+
+class Vehicle {
+    protected:
+        // Atribut
+        int tahunProduksi;
+        string merek;
+        int jumlahRoda;
+        string warna;
+    
+    public:
+        //Ctor
+        Vehicle();
+        Vehicle(int tahunProduksi, string merek, int jumlahRoda, string warna);
+
+        //cctor
+        Vehicle(const Vehicle& other);
+
+        // dtor
+        ~Vehicle();
+
+        // assignment
+        Vehicle& operator=(const Vehicle& other);
+
+        // getter
+        int getTahunProduksi();
+        string getMerek();
+        int getJumlahRoda();
+        string getWarna();
+
+        // setter
+        void setTahunProduksi(int tahunProduksi);
+        void setMerek(string merek);
+        void setJumlahRoda(int jumlahRoda);
+        void setWarna(string warna);
+
+        // Method
+        void printInfo();
+        void drive();        
+
+};
